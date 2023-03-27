fe28bc931635835653c8f3ae4a13cf6060ecadd7
brooks <brooks.houck@gmail.com>
2023-03-27 00:54:22 +0000

    made changes to public rt

--- main.tf
+++ main.tf
@@ -76,9 +76,10 @@ resource "aws_route_table" "public_rt" {
 
   route {
     cidr_block     = var.public_sn_cidr
-    nat_gateway_id = aws_internet_gateway.eureka_igw.id
+    gateway_id = aws_internet_gateway.eureka_igw.id
   }
   tags = {
     Name = var.public_rt_name
   }
 }
+
