798557e655ba98a0de3901a865987e9243c9e858
brooks <brooks.houck@gmail.com>
2023-03-27 00:30:22 +0000

    adding variables

---
+++ terraform.tfvars
@@ -0,0 +1,16 @@
+## VPC & Subnet Variables ##
+
+vpc_name = "Eureka Productions"
+vpc_cidr = "12.0.0.0/16"
+
+public_sn_name = "Eurkea Public Subnet"
+public_sn_cidr = "12.0.0.0/18"
+
+private_sn_name = "Eureka Private Subnet"
+private_sn_cidr = "12.0.192.0/19"
+
+igw_name    = "Eureka IGW"
+nat_gw_name = "Eurka NAT GW"
+
+private_rt_name = "Private Route Table"
+public_rt_name  = "Public Route Table"
---
+++ variables.tf
@@ -0,0 +1,35 @@
+var "vpc_name" {
+  type = string
+}
+
+var "public_sn_name" {
+  type = string
+}
+
+var "vpc_cidr" {
+  type = string
+}
+
+var "public_sn_cidr" {
+  type = string
+}
+
+var "private_sn_name" {
+  type = string
+}
+
+var "private_sn_cidr" {
+  type = string
+}
+
+var "igw_name" {
+  type = string
+}
+
+var "public_rt_name" {
+  type = string
+}
+
+var "private_rt_name" {
+  type = string
+}
